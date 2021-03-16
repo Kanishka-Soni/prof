e07dc01eff52d83e1eea94f99a719658f0486c2d
Kanishka-Soni <kanishka.soni16@gmail.com>
2021-03-16 18:37:08 +0530

    gsrgs

--- main.py
+++ main.py
@@ -1,13 +1,245 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+# import random
+# from flask import jsonify
+# from distutils.util import strtobool
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from sqlalchemy.orm import relationship
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+
+app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-app = Flask(__name__) #intance of our flask application 
 
-#Route '/' to facilitate get request from our flutter app
-@app.route('/', methods = ['GET'])
-def index():
-    return jsonify({'greetings' : 'Hi! this is python'}) 
+# Connect to Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=47678)
+# Cafe TABLE Configuration
+class User(UserMixin, db.Model):
+    _tablename_ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+    posts = relationship("BlogPost", back_populates="author")
+    comments = relationship("Comment", back_populates="comment_author")
+
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+
+    def to_dict(self):
+        # Method 1.
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            # Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
+'''
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+
+        if User.query.filter_by(email=form.email.data).first():
+            print(User.query.filter_by(email=form.email.data).first())
+            # User already exists
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for('login'))
+
+        hash_and_salted_password = generate_password_hash(
+            form.password.data,
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+        new_user = User(
+            email=form.email.data,
+            name=form.name.data,
+            password=hash_and_salted_password,
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for("get_all_posts"))
+
+    return render_template("register.html", form=form, current_user=current_user, year=year)
+'''
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        request_data = request.data
+        request_data = json.loads(request_data.decode('utf-8'))
+        name = request_data["name"]
+        print(name)
+        password = request_data["password"]
+        print(password)
+
+        hash_and_salted_password = generate_password_hash(
+            password,
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+        new_user = User(
+            name=name,
+            password=hash_and_salted_password,
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return ''
+
+    return jsonify({"name": name, "password": hash_and_salted_password})
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+
+        user = User.query.filter_by(email=email).first()
+        # Email doesn't exist or password incorrect.
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+        else:
+            login_user(user)
+            return redirect(url_for('get_all_posts'))
+    return render_template("login.html", form=form, current_user=current_user, year=year)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('get_all_posts'))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+# HTTP GET - Read Record
+@app.route("/random")
+def random_cafe():
+    all_cafes = db.session.query(Cafe).all()
+    random_cafe = random.choice(all_cafes)
+    # Method 1
+    # return jsonify(cafe={
+    #     # Omit the id from the response
+    #     # "id": random_cafe.id,
+    #     "name": random_cafe.name,
+    #     "map_url": random_cafe.map_url,
+    #     "img_url": random_cafe.img_url,
+    #     "location": random_cafe.location,
+
+    #     # Put some properties in a sub-category
+    #     "amenities": {
+    #         "seats": random_cafe.seats,
+    #         "has_toilet": random_cafe.has_toilet,
+    #         "has_wifi": random_cafe.has_wifi,
+    #         "has_sockets": random_cafe.has_sockets,
+    #         "can_take_calls": random_cafe.can_take_calls,
+    #         "coffee_price": random_cafe.coffee_price,
+    #     }
+    # })
+    # Method 2
+    return jsonify(cafe=random_cafe.to_dict())
+    # Creating a dictionary and using Jsonify on the dictionary
+
+
+@app.route("/all")
+def all():
+    all_cafes = db.session.query(Cafe).all()
+    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+
+
+@app.route("/search")
+def get_cafe_at_location():
+    query_location = request.args.get("loc")
+    cafes = db.session.query(Cafe).filter_by(location=query_location).all()
+    if cafes:
+        return jsonify(cafe=[cafe.to_dict() for cafe in cafes])
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
+
+    # HTTP POST - Create Record
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    new_cafe = Cafe(
+        id=request.form.get("id"),
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("location"),
+        seats=request.form.get("seats"),
+        has_toilet=bool(strtobool(request.form.get("has_toilet"))),
+        has_wifi=bool(strtobool(request.form.get("has_wifi"))),
+        has_sockets=bool(strtobool(request.form.get("has_sockets"))),
+        can_take_calls=bool(strtobool(request.form.get("can_take_calls"))),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"Success": "Successfully Added new cafe"})
+
+    # HTTP PUT/PATCH - Update Record
+
+
+@app.route("/update-price/<int:id>", methods=["PATCH"])
+def updatePrice(id):
+    find_cafe_from_id = Cafe.query.get(id)
+    if find_cafe_from_id:
+        find_cafe_from_id.coffee_price = request.args.get("coffee_price")
+        db.session.commit()
+        return jsonify(response={"Success": "Successfully changed the coffee price"})
+    else:
+        return jsonify(response={"Failure": "Not Changed"})
+
+    # HTTP DELETE - Delete Record
+
+
+@app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
+def delete_cafe(cafe_id):
+    api_key = request.args.get("api-key")
+    if api_key == "ASDFG":
+        cafe = db.session.query(Cafe).get(cafe_id)
+        if cafe:
+            db.session.delete(cafe)
+            db.session.commit()
+            return jsonify(response={"success": "Successfully deleted the cafe from the database."}), 200
+        else:
+            return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
+    else:
+        return jsonify(error={"Forbidden": "Sorry, that's not allowed. Make sure you have the correct api_key."}), 403
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
